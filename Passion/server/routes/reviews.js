49c1343bd9bc6aebf46efe242e18324942fbf261
tdude0175 <tdude0175@gmail.com>
2019-05-31 14:40:39 -0500

    basic level CRUD for reviews has been added

--- Passion/server/routes/reviews.js
+++ Passion/server/routes/reviews.js
@@ -4,5 +4,88 @@ const ReviewCollection = require("../models/reviewSchema.js");
 const GameCollection = require("../models/gameSchema.js");
 
 
+router.get("/reviews",(req,res)=>
+{
+   res.send("we have review access now");
+});
+
+
+//don't need to fill out each individuial piece since info can be added late {and will be}
+router.post("/addReview",(req,res)=>
+{
+    ReviewCollection.create(req.body,(errors,results)=>
+    {
+        if(errors)
+        {
+            res.send(errors);
+        }
+        else
+            {
+                console.log("it didn't have an error");
+                res.send(results._id);
+            }
+    });
+});
+
+router.post("/listReviews",(req,res)=>
+{
+    ReviewCollection.find({gameIdReferencePiece:req.body.gameIdReferencePiece},(errors,results)=>
+    {
+        if(errors)
+        {
+            res.send(errors);
+        }
+        else
+            {
+                res.send(results)
+            }
+    });
+});
+
+router.put("/updateReview",(req,res)=>
+{
+    ReviewCollection.findOneAndUpdate({_id:req.body._id},req.body,(errors,results)=>
+    {
+        if(errors)
+        {
+            res.send(errors);
+        }
+        else
+            {
+                res.send("Update complete");
+            }
+    });
+});
+
+router.put("/reviewReview",(req,res)=>
+{
+    ReviewCollection.findOneAndUpdate({_id:req.body._id},{$push:{reviewRating:req.body}},(errors,results)=>
+    {
+        if(errors)
+        {
+            res.send(errors);
+        }
+        else
+            {
+                res.send("Review Added");
+            }
+    })
+});
+
+router.delete("/deleteReview",(req,res)=>
+{
+    ReviewCollection.findOneAndDelete({_id: req.body._id},(errors,results)=>
+    {
+        if(errors)
+        {
+            res.send(errors);
+        }
+        else
+            {
+                res.send("Review Removed");
+            }
+    });
+});
+
 
 module.exports = router;
