5dfcb295aed809fb17c71597f5f5ed4e24292283
tdude0175 <tdude0175@gmail.com>
2019-06-05 14:30:52 -0500

    main page styling decent

--- Passion/client/src/App.js
+++ Passion/client/src/App.js
@@ -35,27 +35,61 @@ class App extends Component {
     };
 
     render() {
-        return (
-            <div className="App">
-                <Router>
+        if(this.state.isLoggedIn) {
+            return (
+                <div className="App">
+                    <Router>
 
-                    <header className={"NavBar"}>
-                        <Link to={"/"}><h1>Games lounge</h1></Link>
-                        <Link className={"navPadding"} to={"/YourAccount"}><h3>Account</h3></Link>
-                        <Link className={"navPadding"} to={"/AddGame"}><h3>Add a game?</h3></Link>
-                    </header>
+                        <header className={"NavBar"}>
+                            <Link to={"/"}><h1>Games lounge</h1></Link>
+                            {/*<Link className={"navPadding"} to={"/YourAccount"}><h3>Account</h3></Link>*/}
+                            <Link className={"navPadding"} to={"/AddGame"}><h3>Add a game?</h3></Link>
+                        </header>
 
-                    <Route exact path={"/"} component={() => <GameList useGameId={this.useGameId}/>}/>
-                    <Route path={"/YourAccount"} component={()=><UserAccount loggingIn={this.loggingIn} isLoggedIn={this.state.isLoggedIn} username={this.state.username}/>}/>
-                    <Route path={"/GameReviewPage"} component={() => <ReviewGame/>}/>
-                    <Route path={"/AddGame"} component={() => <AddGame/>}/>
-                    <Route path={"/GameInfo"} component={()=> <GameInfo isLoggedIn={this.state.isLoggedIn} _id={this.state.gameId}/>}/>
-                    <Route path={"/AddAReview"} component={()=><ReviewGame userId={this.state.userId} isLoggedIn={this.state.isLoggedIn} gameId={this.state.gameId}/>}/>
-                </Router>
+                        <Route exact path={"/"} component={() => <GameList useGameId={this.useGameId}/>}/>
+                        <Route path={"/YourAccount"} component={() => <UserAccount loggingIn={this.loggingIn}
+                                                                                   isLoggedIn={this.state.isLoggedIn}
+                                                                                   username={this.state.username}/>}/>
+                        <Route path={"/GameReviewPage"} component={() => <ReviewGame/>}/>
+                        <Route path={"/AddGame"} component={() => <AddGame/>}/>
+                        <Route path={"/GameInfo"} component={() => <GameInfo isLoggedIn={this.state.isLoggedIn}
+                                                                             _id={this.state.gameId}/>}/>
+                        <Route path={"/AddAReview"} component={() => <ReviewGame userId={this.state.userId}
+                                                                                 isLoggedIn={this.state.isLoggedIn}
+                                                                                 gameId={this.state.gameId}/>}/>
+                    </Router>
 
 
-            </div>
-        );
+                </div>
+            );
+        }else{
+            return (
+                <div className="App">
+                    <Router>
+
+                        <header className={"NavBar"}>
+                            <Link to={"/"}><h1>Games lounge</h1></Link>
+                            <Link className={"navPadding"} to={"/YourAccount"}><h3>Account</h3></Link>
+                            {/*<Link className={"navPadding"} to={"/AddGame"}><h3>Add a game?</h3></Link>*/}
+                        </header>
+
+                        <Route exact path={"/"} component={() => <GameList useGameId={this.useGameId}/>}/>
+                        <Route path={"/YourAccount"} component={() => <UserAccount loggingIn={this.loggingIn}
+                                                                                   isLoggedIn={this.state.isLoggedIn}
+                                                                                   username={this.state.username}/>}/>
+                        <Route path={"/GameReviewPage"} component={() => <ReviewGame/>}/>
+                        <Route path={"/AddGame"} component={() => <AddGame/>}/>
+                        <Route path={"/GameInfo"} component={() => <GameInfo isLoggedIn={this.state.isLoggedIn}
+                                                                             _id={this.state.gameId}/>}/>
+                        <Route path={"/AddAReview"} component={() => <ReviewGame userId={this.state.userId}
+                                                                                 isLoggedIn={this.state.isLoggedIn}
+                                                                                 gameId={this.state.gameId}/>}/>
+                    </Router>
+
+
+                </div>
+            )
+        }
     }
 }
 
